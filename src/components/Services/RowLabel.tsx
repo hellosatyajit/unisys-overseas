b4054de2677b593966007fa67ab37d97c1fd00f1
Satyajit Chaudhary <thisissatyajit05@gmail.com>
2025-06-02 13:57:09 +0530

    fix

--- src/components/Services/RowLabel.tsx
+++ src/components/Services/RowLabel.tsx
@@ -1,12 +1,12 @@
 'use client'
-import { Service } from '@/payload-types'
+import { Service, ServicesCollection } from '@/payload-types'
 import { RowLabelProps, useRowLabel } from '@payloadcms/ui'
 
 export const RowLabel: React.FC<RowLabelProps> = () => {
   const data = useRowLabel<NonNullable<Service['services']>[number]>()
 
-  const label = data?.data?.title
-    ? `Service item ${data.rowNumber !== undefined ? data.rowNumber + 1 : ''}: ${data?.data?.title}`
+  const label = (data?.data?.service as ServicesCollection)?.title
+    ? `Service item ${data.rowNumber !== undefined ? data.rowNumber + 1 : ''}: ${(data?.data?.service as ServicesCollection)?.title}`
     : 'Row'
 
   return <div>{label}</div>
